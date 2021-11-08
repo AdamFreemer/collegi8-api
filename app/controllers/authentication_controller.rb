702445d82934f63bff60871062b08ee07b8bc13d
Adam Freemer <adam@freemer.com>
2021-11-07 18:55:07 -0800

    Debug authentication error on heroku

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -12,4 +12,10 @@ class AuthenticationController < ApplicationController
       render json: {}, status: 200
     end
   end
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 end
